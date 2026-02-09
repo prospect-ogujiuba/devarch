8d00a83628527dac60c3100b27cf8d55cc553510
prospect-ogujiuba <prospect.ogujiuba@gmail.com>
2026-02-08 19:01:58 -0500

    feat(08-02): wiring domain package
    - ResolveAutoWires: auto-wire resolution with 0/1/N provider matching
    - InjectEnvVars: env var injection using container DNS pattern
    - ValidateWiring: circular dependency detection via DFS
    - FindOrphanedWires: cleanup for deleted instances

---
+++ api/internal/wiring/env_injector.go
@@ -0,0 +1,29 @@
+package wiring
+
+import (
+	"strconv"
+	"strings"
+
+	"github.com/priz/devarch-api/internal/container"
+)
+
+func InjectEnvVars(stackName string, provider Provider, consumer Consumer) map[string]string {
+	hostname := container.ContainerName(stackName, provider.InstanceName)
+	portStr := strconv.Itoa(provider.Port)
+	protocol := provider.Protocol
+	name := provider.ContractName
+
+	injections := make(map[string]string)
+
+	for envKey, template := range consumer.EnvVars {
+		value := template
+		value = strings.ReplaceAll(value, "{{hostname}}", hostname)
+		value = strings.ReplaceAll(value, "{{port}}", portStr)
+		value = strings.ReplaceAll(value, "{{protocol}}", protocol)
+		value = strings.ReplaceAll(value, "{{name}}", name)
+
+		injections[envKey] = value
+	}
+
+	return injections
+}
